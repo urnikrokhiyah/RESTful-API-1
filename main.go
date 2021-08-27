02468e0e68891624ba81ef3b67b481b4ac2dc4c6
urnikrokhiyah <53650964+urnikrokhiyah@users.noreply.github.com>
2021-08-27 16:35:11 +0700

    Update main.go

--- main.go
+++ main.go
@@ -7,65 +7,6 @@ import (
 	"github.com/labstack/echo"
 )
 
-// // func HelloController(c echo.Context) error {
-// // 	return c.String(http.StatusOK, "Hello world")
-// // }
-
-// // type User struct {
-// // 	Id    int
-// // 	Name  string
-// // 	Email string
-// // }
-
-// // func getUser(c echo.Context) error {
-// // 	user := User{Name: "urnik", Email: "urnik@gmail.com"}
-// // 	return c.JSON(http.StatusOK, user)
-// // }
-
-// // func getUserController(c echo.Context) error {
-// // 	id, _ := strconv.Atoi(c.Param("id"))
-// // 	user := User{Id: id, Name: "urnik", Email: "urnik@gmail.com"}
-// // 	return c.JSON(http.StatusOK, map[string]interface{}{"user": user})
-// // }
-
-// // func userSearch(c echo.Context) error {
-// // 	match := c.QueryParam("match")
-// // 	return c.JSON(http.StatusOK, map[string]interface{}{"match": match, "result": []string{"adi", "aan", "asif"}})
-// // }
-
-// // func createUser(c echo.Context) error {
-// // 	name := c.FormValue("name")
-// // 	email := c.FormValue("email")
-
-// // 	var user Users
-// // 	user.Name = name
-// // 	user.Emailuser = email
-
-// // 	return c.JSON(http.StatusOK, map[string]interface{}{"user": user})
-// // }
-
-// type Users struct {
-// 	Name  string `json:"name" form:"name"`
-// 	Email string `json:"email" form:"email`
-// }
-
-// func createUsers(c echo.Context) error {
-// 	user := Users{}
-// 	c.Bind(&user)
-
-// 	return c.JSON(http.StatusOK, map[string]interface{}{"messages": "success", "user": user})
-// }
-
-// func main() {
-// 	e := echo.New()
-// 	// e.GET("/", HelloController)
-// 	// e.GET("/user", getUser)
-// 	// e.GET("/user/:id", getUserController)
-// 	// e.GET("/user", userSearch)
-// 	e.POST("/user", createUsers)
-// 	e.Start(":8000")
-// }
-
 type User struct {
 	Id       int    `json: "id" form: "id"`
 	Name     string `json: "name" form: "name"`
